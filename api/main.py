fbdb6dcc65b78c46173b311fb6aff2b43db2c939
Felipe Ranzoni Borges <felipe.ranzoni@hotmail.com>
2025-10-12 09:18:44 -0300

    Melhorias no código

--- api/main.py
+++ api/main.py
@@ -2,7 +2,7 @@ import uvicorn
 import os
 
 from dotenv import load_dotenv
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from fastapi.middleware.cors import CORSMiddleware
 
 from brain import talk
@@ -17,7 +17,7 @@ load_dotenv()
 __limit_messages_to_persona = int(os.getenv('LIMIT_MESSAGES_TO_PERSONA'))
 __limit_messages_to_response = int(os.getenv('LIMIT_MESSAGES_TO_RESPONSE'))
 
-__persona_data = PersonasData()
+__personas_data = PersonasData()
 
 app = FastAPI()
 app.add_middleware(
@@ -28,8 +28,16 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
+def __handle_unauthorized(response: Response, message: str) -> BaseResponse:
+    response.status_code = 401
+    return fail_response(message)
+
+def __handle_bad_request(response: Response, message: str) -> BaseResponse:
+    response.status_code = 400
+    return fail_response(message)
+
 @app.post('/generate-id')
-def generate_id() -> BaseResponse:
+def generate_id(response: Response) -> BaseResponse:
     try:
         id = generate_random_id()
 
@@ -38,12 +46,15 @@ def generate_id() -> BaseResponse:
             source=id
         )
     except Exception as e:
-        return fail_response(f'Fail to generate the ID: {e}')
+        return __handle_bad_request(
+            response=response,
+            message=f'Fail to generate the ID: {e}'
+        )
 
 @app.post('/talk/{id}/{persona_id}')
-def talk_with_persona(id: str, persona_id: int, talk_request: TalkRequest) -> BaseResponse:
+def talk_with_persona(id: str, persona_id: int, talk_request: TalkRequest, response: Response) -> BaseResponse:
     try:
-        persona = __persona_data.get_by_id(persona_id)
+        persona = __personas_data.get_by_id(persona_id)
 
         history = HistoryConversation(id, persona.id())
         messages_history = history.get_history(limit=__limit_messages_to_persona)
@@ -58,52 +69,70 @@ def talk_with_persona(id: str, persona_id: int, talk_request: TalkRequest) -> Ba
 
         return persona_message_to_response(persona_message)
     except Exception as e:
-        return fail_response(f'Fail to talk with the persona: {e}')
+        return __handle_bad_request(
+            response=response,
+            message=f'Fail to talk with the persona: {e}'
+        )
 
 @app.get('/messages/{id}/{persona_id}')
-def get_messages(id: str, persona_id: int) -> BaseResponse:
+def get_messages(id: str, persona_id: int, response: Response) -> BaseResponse:
     try:
-        persona = __persona_data.get_by_id(persona_id)
+        persona = __personas_data.get_by_id(persona_id)
 
         history = HistoryConversation(id, persona.id())
         messages_history = history.get_history(limit=__limit_messages_to_response)
 
         return messages_history_to_response(messages_history)
     except Exception as e:
-        return fail_response(f'Fail to get the history messages: {e}')
+        return __handle_bad_request(
+            response=response,
+            message=f'Fail to get the history messages: {e}'
+        )
 
 @app.get('/personas')
-def get_personas():
+def get_personas(response: Response):
     try:
-        personas = __persona_data.get_all()
+        personas = __personas_data.get_all()
         return personas_list_to_response(personas)
     except Exception as e:
-        return fail_response(f'Fail to get the personas: {e}')
+        return __handle_bad_request(
+            response=response,
+            message=f'Fail to get the personas: {e}'
+        )
     
 @app.post('/persona')
-def create_persona(persona_request: PersonaRequest, request: Request) -> BaseResponse:
+def create_persona(persona_request: PersonaRequest, request: Request, response: Response) -> BaseResponse:
     try:
         secret_key = request.headers.get("X-Secret-Key")
         if not validate_secret_key(secret_key):
-            return fail_response(f'Access unauthorized')
+            return __handle_unauthorized(
+                response=response,
+                message=f'Access unauthorized'
+            )
 
-        persona_created = __persona_data.include_persona(
+        persona_created = __personas_data.include_persona(
             name=persona_request.name,
             prompt=persona_request.prompt
         )
 
         return persona_to_response(persona_created)
     except Exception as e:
-        return fail_response(f'Fail to create a new persona: {e}')
+        return __handle_bad_request(
+            response=response,
+            message=f'Fail to create a new persona: {e}'
+        )
     
 @app.put('/persona/{id}')
-def update_persona(id: int, persona_request: PersonaRequest, request: Request) -> BaseResponse:
+def update_persona(id: int, persona_request: PersonaRequest, request: Request, response: Response) -> BaseResponse:
     try:
         secret_key = request.headers.get("X-Secret-Key")
         if not validate_secret_key(secret_key):
-            return fail_response(f'Access unauthorized')
+            __handle_unauthorized(
+                response=response,
+                message=f'Access unauthorized'
+            )
         
-        persona_updated = __persona_data.update_persona(
+        persona_updated = __personas_data.update_persona(
             id=id,
             name=persona_request.name,
             prompt=persona_request.prompt
@@ -113,7 +142,10 @@ def update_persona(id: int, persona_request: PersonaRequest, request: Request) -
 
         return persona_to_response(persona_updated)
     except Exception as e:
-        return fail_response(f'Fail to create a new persona: {e}')
+        return __handle_bad_request(
+            response=response,
+            message=f'Fail to update the persona: {e}'
+        )
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='127.0.0.1', port=8000, reload=True, log_level='debug')
